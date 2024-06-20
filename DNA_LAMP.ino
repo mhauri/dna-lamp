4e258c01f5c014d30c41b1b13db17885a73d9ff8
Marcel Hauri <marcel.hauri@gmail.com>
2024-06-20 14:45:10 +0200

    [task] finish led and motor, add reset

--- DNA_LAMP.ino
+++ DNA_LAMP.ino
@@ -1,61 +1,88 @@
 #include <Stepper.h>
 
-
-#define MOTOR_STEPS 2048
-
+// PINS
 #define MOTOR_IN1 13
 #define MOTOR_IN2 12
 #define MOTOR_IN3 14
 #define MOTOR_IN4 27
 #define MODE_1 26
 #define MODE_2 25
-#define LED_CONTROL 2
+#define LED_PWM 15
 
-const int ledPin = 16;
-const int controlPin = 2;
-const int buttonPin = 17;
-const int freq = 5000;
-const int resolution = 8;
 
-int brightness = 0;
-int fadeAmount = 5;
+// STEPPER MOTOR SETTINGS
+#define MOTOR_STEPS 2048
+#define MOTOR_SPEED 10
+#define MOTOR_PERIOD 20
 
-// initialize the stepper library
+// LED'S
+#define FADE_AMOUNT 5
+#define LED_PERIOD 50
+#define LED_PAUSE 8000
+
+// INITIALIZE STEPPER LIBRARY
 Stepper myStepper(MOTOR_STEPS, MOTOR_IN1, MOTOR_IN3, MOTOR_IN2, MOTOR_IN4);
+
+// VARIABLES 
+int brightness = 0;
+int fadeAmount = FADE_AMOUNT;
+unsigned long ledTime = 0;
+const unsigned long ledPeriod = LED_PERIOD;
+const unsigned long ledPause = LED_PAUSE;
+unsigned long motorTime = 0;
+const unsigned long motorPeriod = MOTOR_PERIOD;
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Serial Communication established!");
-  myStepper.setSpeed(5);
+  myStepper.setSpeed(MOTOR_SPEED);
   pinMode(MODE_1, INPUT_PULLUP);
   pinMode(MODE_2, INPUT_PULLUP);
+  pinMode(LED_PWM, OUTPUT);
 }
 
 void loop() {
   int modeOne = digitalRead(MODE_1);
   int modeTwo = digitalRead(MODE_2);
-  Serial.println("HELLO WORLD");
 
-  if(modeOne == HIGH) {
-    Serial.println("Mode 1: ON");
+  if(modeOne == LOW) {
+    dimmLeds();
+  }
+  if(modeTwo == LOW) {
+    dimmLeds();
+    rotate();
   }
 
-  if(modeTwo == HIGH) {
-    Serial.println("Mode 2: ON");
+  if(modeOne == HIGH && modeTwo == HIGH) {
+    reset();
   }
 }
 
 void dimmLeds() {
-    ledcWrite(ledPin, brightness);
-    ledcWrite(controlPin, brightness);		
-
-    brightness = brightness + fadeAmount;
-    if (brightness == 0 || brightness == 255) {
-      fadeAmount = -fadeAmount ; 
+    unsigned long current = millis();
+    if(current - ledTime >= ledPeriod) {
+      ledTime = current;
+      brightness = brightness + fadeAmount;
+      if (brightness == 0 || brightness == 255) {
+        fadeAmount = -fadeAmount;
+      }
+      analogWrite(LED_PWM, brightness);
     }
 }
 
 void rotate() {
-  Serial.println("rotate");
-  myStepper.step(MOTOR_STEPS);
+  unsigned long current = millis();
+  if(current - motorTime >= motorPeriod) {
+    motorTime = current;
+    myStepper.step(1);
+  }
+}
+
+void reset() {
+  brightness = 5;
+  analogWrite(LED_PWM, 0);
+  digitalWrite(MOTOR_IN1, 0);
+  digitalWrite(MOTOR_IN2, 0);
+  digitalWrite(MOTOR_IN3, 0);
+  digitalWrite(MOTOR_IN4, 0);
 }
